b3dad3d125779613e2194b1d8c416576d2563167
ZR1ck <100997608+ZR1ck@users.noreply.github.com>
2025-09-04 21:35:07 +0700

    feat: update apply command
    * feat: update  command
    
    * fix: wrap text space error

--- src/commands/apply.rs
+++ src/commands/apply.rs
@@ -1,13 +1,16 @@
-use std::cmp;
-
 use console::style;
+use dialoguer::{Input, theme::ColorfulTheme};
 
 use crate::core::{header::get_header, storage::Storage};
 
-pub fn run(name: String, values: Vec<String>, store: &mut Storage) {
+pub fn run(name: String, store: &mut Storage, input_theme: &ColorfulTheme) {
     if let Some(mut template) = store.get(&name).cloned() {
-        for i in 0..cmp::min(template.fields.len(), values.len()) {
-            template.fields[i].default_value = values[i].clone();
+        for field in &mut template.fields {
+            let input = Input::with_theme(input_theme)
+                .with_prompt(field.key.clone())
+                .interact()
+                .unwrap();
+            field.default_value = input;
         }
         get_header(&template);
     } else {
